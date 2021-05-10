785af086dd4166b90b7c1a1047766e5c333d7891
ryosuke-kamiya <cinesuija@gmail.com>
2021-05-10 23:44:50 +0900

    update: reducersを書いてみる

---
+++ src/reducks/users/reducers.js
@@ -0,0 +1,14 @@
+import * as Actions from './actions'
+import initialState from '../store/initialState'
+
+export const UsersReducer = (state = initialState.users, action) => {//第一引数は、現在の状態または初期状態、第２引数はactionがreturnした値
+    switch (action.type) {
+        case Actions.SIGN_IN:
+            return {
+                ...state,//現在のstateの状態はそのままに
+                ...action.payload//アクションで指定があったもののみ上書きする
+            }
+        default:
+            break;
+    }
+}
