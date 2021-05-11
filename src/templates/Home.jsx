4f4e4eaf2fa973766ced1d449b00df7a532c601a
ryosuke-kamiya <cinesuija@gmail.com>
2021-05-11 23:48:27 +0900

    update: re-ducxsパターンを学んだ

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getUserId } from "../reducks/users/selectors";
+
 
 const Home = () => {
+    const selector = useSelector(state => state);//全体のstateを取得する
+    const uid = getUserId(selector)
+
     return(
+        <div>
         <h2>home</h2>
+        <div>{uid}</div>
+        </div>
+
     )
 }
 
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import {push} from "connected-react-router"
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { signInAction } from "../reducks/users/actions";
 
-const Login = () => {  
+const Login = () => {
     const dispatch = useDispatch()
-    const selector = useSelector(state=> state)
 
-    console.log(selector.router);
     return(
         <div>
             <h2>ログイン</h2>
-            <button onClick={() => dispatch(push("/"))}>ログインする</button>
+            <button onClick={() => {
+            dispatch(signInAction({uid: "0001", username: "testname"}))
+            dispatch(push("/"))
+            }}>ログインする</button>
         </div>
     )
 
